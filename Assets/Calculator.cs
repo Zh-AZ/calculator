2d3b74d28dac0488a19ce37c8f066c17340522b1
Zh-AZ <zhaparovaziz03@gmail.com>
2024-09-04 15:07:34 +0600

    number work

---
+++ Assets/Calculator.cs
@@ -0,0 +1,171 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculator : MonoBehaviour
+{
+    public Text DisplayText;
+
+    private bool isSecondOperand = true;
+    private bool isMathOperation = false;
+    private bool isEquals = false;
+    private bool isDivision = false;
+    private bool isMinus = false;
+    private bool isPlus = false;
+    private double firstOperand;
+    private double secondOperand;
+    private double result;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnButtonClickZero() { EnterNumbers(0); }
+
+    public void OnButtonClickOne() { EnterNumbers(1); }
+
+    public void OnButtonClickTwo() { EnterNumbers(2); }
+
+    public void OnButtonClickThree() { EnterNumbers(3); }
+
+    public void OnButtonClickFour() { EnterNumbers(4); }
+
+    public void OnButtonClickFive() {EnterNumbers(5);}
+
+    public void OnButtonClickSix() {EnterNumbers(6);}
+
+    public void OnButtonClickSeven() {EnterNumbers(7);}
+
+    public void OnButtonClickEight() {EnterNumbers(8);}
+
+    public void OnButtonClickNine() {EnterNumbers(9);}
+
+    public void OnButtonClickFloatingPoint()
+    {
+        bool isPointExist = false;
+
+        if (DisplayText.text != string.Empty)
+        {
+            foreach (char letter in DisplayText.text)
+            {
+                if(letter == '.')    
+                {
+                    isPointExist = false;
+                    break; 
+                }
+                else
+                {
+                    isPointExist = true;
+                }
+            }
+
+            if (isPointExist) { DisplayText.text += "."; }
+        }
+        else
+        {
+            DisplayText.text = "0.";
+        }
+    }
+
+    private void EnterNumbers(int num)
+    {
+        if (DisplayText.text != string.Empty && DisplayText.text != 0.ToString())
+        {
+            if (isMathOperation)
+            {
+                if (isSecondOperand)
+                {
+                    DisplayText.text = string.Empty;
+                    isSecondOperand = false;
+                }
+                if (isEquals)
+                {
+                    DisplayText.text = string.Empty;
+                    isEquals = false;
+                }
+
+                DisplayText.text += num;
+                secondOperand = Convert.ToDouble(DisplayText.text);
+            }
+            else
+            {
+                DisplayText.text += num;
+                firstOperand = Convert.ToDouble(DisplayText.text);
+            }
+        }
+        else
+        {
+            DisplayText.text = num.ToString();
+        }
+    }
+
+    public void ResultPercentage()
+    {
+        if(isMathOperation)
+        {
+            double currentNum = Convert.ToDouble(DisplayText.text);
+            double percentageOfNumber = currentNum * 0.10;
+            secondOperand = percentageOfNumber;
+        }
+    }
+
+    public void OnOrReset() 
+    {
+        DisplayText.text = "0";
+        isSecondOperand = true;
+        isMathOperation = false;
+    }
+
+    public void Division() { Operator(ref isDivision); }
+
+    public void Minus() { Operator(ref isMinus); }
+
+    public void Plus() { Operator(ref isPlus); }
+
+    private void Operator(ref bool isOperatorDone)
+    {
+        if (DisplayText.text != string.Empty)
+        {
+            isMathOperation = true;
+            isOperatorDone = true;
+            isSecondOperand = true;
+            firstOperand = Convert.ToDouble(DisplayText.text);
+        }
+    }
+
+    public void Equals()
+    {
+        if (isDivision)
+        {
+            result = firstOperand / secondOperand;
+        }
+        else if (isMinus)
+        {
+            result = firstOperand - secondOperand;
+        }
+        else if (isPlus)
+        {
+            result = firstOperand + secondOperand;
+        }
+        
+        DisplayText.text = result.ToString();
+        isEquals = true;
+        isMathOperation = true;
+        isSecondOperand = false;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+ 
